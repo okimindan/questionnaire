9216e4393009baf2b36add98b07a700847eb465e
okimindan <84608687+okimindan@users.noreply.github.com>
2025-08-29 20:50:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
+
+console.log('[ENV]', {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER
+});
+
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
